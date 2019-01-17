fe2eee9f33b76f7ec0ef767899405b26ec706609
Aclik <1253668861@qq.com>
2019-01-17 16:17:55 +0800

    SpringCloud Config 客户端

---
+++ spring-cloud-config-client-3355/src/main/java/com/yxbuild/CloudConfigApplication.java
@@ -0,0 +1,17 @@
+package com.yxbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: WenxiangChen
+ * @Description:
+ * @Date: Create in 15:44 2019/1/17
+ * @Modified By:
+ */
+@SpringBootApplication
+public class CloudConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigApplication.class,args);
+    }
+}
---
+++ spring-cloud-config-client-3355/src/main/java/com/yxbuild/controller/ConfigClientController.java
@@ -0,0 +1,32 @@
+package com.yxbuild.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: WenxiangChen
+ * @Description:
+ * @Date: Create in 15:41 2019/1/17
+ * @Modified By:
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value(value = "${spring.application.name}")
+    private String applicationName;
+
+    @Value("${eureka.client.service-url.defaultZone}")
+    private String eurekaServers;
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/config")
+    public String getConfig() {
+        String str = "applicationName: " + applicationName + "\t eurekaServers:" + eurekaServers + "\t port: " + port;
+        System.out.println("******str: " + str);
+        return "applicationName: " + applicationName + "\t eurekaServers:" + eurekaServers + "\t port: " + port;
+    }
+}
